b07250231107d6b0aeb43d899e2f1d2f205e26ee
YosefAshenafi <7629076+YosefAshenafi@users.noreply.github.com>
2022-09-10 06:12:30 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long long longlongType;
 	char charType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
