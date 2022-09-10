729883f702a3eeb33998ac981ce5180f42530cc7
YosefAshenafi <7629076+YosefAshenafi@users.noreply.github.com>
2022-09-10 03:06:37 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print digits
+ *
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i+'0');
+	}
+	putchar('\n');
+
+	return (0);
+}
