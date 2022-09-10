74dcc7f7e91380311c29bd6bd88be2b5a5093ebb
YosefAshenafi <7629076+YosefAshenafi@users.noreply.github.com>
2022-09-09 23:39:24 -0700

    trailing whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /*
- * main - Entry point 
+ * main - Entry point
  *
- * Description: 'the program's description' 
+ * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
