ac1fd08464c445bf9159ecc0cea0e5f6c96bac52
YosefAshenafi <7629076+YosefAshenafi@users.noreply.github.com>
2022-09-10 05:50:04 -0700

    works

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - return string
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
