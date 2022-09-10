b3d261c2efa4bb4c3f83b4503be8435c914716d4
YosefAshenafi <7629076+YosefAshenafi@users.noreply.github.com>
2022-09-10 02:30:00 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print alphabet
+ *
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+	{
+		putchar(alphabet);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; ++alphabet)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
